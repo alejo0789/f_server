ee2fa833ac68df9520ce677fdae48618fad2ed9f
unknown <alejo0789@hotmail.com>
2023-04-16 13:45:21 -0500

    antes de borrar los archivos locales

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, jsonify,send_file, request
+from flask import Flask, render_template, jsonify,send_file, request,send_from_directory
 import os
 from datetime import datetime
 import requests
 import glob
 import fnmatch
+import urllib.parse
 
 from src.components.savexls import guardar_en_excel
 
@@ -56,16 +57,11 @@ def listar_archivos():
 
 @app.route('/descargar/<filename>')
 def descargar_archivo(filename):
-    archivo_path = 'src/files/' + filename
-    return send_file(archivo_path, as_attachment=True)
+    archivo_path = 'src/files/3105487076/' + filename
+    return send_file(archivo_path, as_attachment=False)
 
-@app.route('/ver_archivo/<path:archivo>')
-def ver_archivo(archivo):
-    return render_template('ver_archivo.html', archivo=archivo)
 
-@app.route('/mostrar_archivo/<path:archivo>')
-def mostrar_archivo(archivo):
-    return send_file(archivo)
+
 
 
 from datetime import datetime
@@ -74,20 +70,48 @@ import os
 import glob
 import fnmatch
 
-@app.route('/buscar_archivos')
-def buscar_archivos():
-    path = "src/files"  # replace with the path to your folder
-    nombre_archivo = request.args.get('nombre', '')
+import os
+import glob
+import fnmatch
+import re
+from datetime import datetime
+@app.route('/buscar_archivos/<folder>')
+def buscar_archivos(folder):
+    path = "src/files/"+folder  # replace with the path to your folder
+    fecha = request.args.get('mes', '')
+    
+    trimestres = {
+        'enero': 'ene-mar',
+        'febrero': 'ene-mar',
+        'marzo': 'ene-mar',
+        'abril': 'abr-jun',
+        'mayo': 'abr-jun',
+        'junio': 'abr-jun',
+        'julio': 'jul-sep',
+        'agosto': 'jul-sep',
+        'septiembre': 'jul-sep',
+        'octubre': 'oct-dic',
+        'noviembre': 'oct-dic',
+        'diciembre': 'oct-dic',
+    }
+    
+    mes_regex = r'\d{9}_(' + '|'.join(trimestres.values()) + ')\.xlsx$'
+    
     files = []
     for file_path in glob.glob(os.path.join(path, '*')):
-        if os.path.isfile(file_path) and fnmatch.fnmatch(os.path.basename(file_path), f'*{nombre_archivo}*'):
-            modified_time = os.path.getmtime(file_path)
-            modified_time_str = datetime.fromtimestamp(modified_time).strftime("%d/%m/%Y")
-            files.append({
-                'name': os.path.basename(file_path),
-                'modified_time': modified_time_str,
-                'file_path': file_path,
-            })
+        if os.path.isfile(file_path):
+            match = re.search(mes_regex, os.path.basename(file_path))
+            if match:
+                trimestre = match.group(1)
+                modified_time = os.path.getmtime(file_path)
+                modified_time_str = datetime.fromtimestamp(modified_time).strftime("%d/%m/%Y")
+                if trimestres.get(fecha) == trimestre:
+                    files.append({
+                        'name': os.path.basename(file_path),
+                        'modified_time': modified_time_str,
+                        'trimestre': trimestre,
+                        'file_path': file_path,
+                    })
     return render_template('archivos.html', archivos=files)
 
 @app.route('/register', methods=['POST'])
@@ -103,6 +127,18 @@ def register():
     # Devolver una respuesta JSON
     response = {'message': 'Usuario registrado exitosamente '+ texto}
     return jsonify(response)
+
+
+@app.route('/ver_archivos/<archivo>')
+def ver_archivos(archivo):
+    archivo = request.args.get('archivo', '')
+   # nombre_archivo = os.path.basename(archivo)
+    directorio_archivo = os.path.dirname(archivo)
+   
+    return send_from_directory(directory='src/files/3105487076', path= "3105487076_ene-mar.xlsx")
+
+
+
 
 
 if __name__ == '__main__':
